b509a2141d3ffdb131e5ef0a5728ce2eb143bc2d
JavaKoala <javakoala1@gmail.com>
2026-04-24 09:30:01 -0400

    Autofocus on large screens

--- src/EventModal.tsx
+++ src/EventModal.tsx
@@ -125,7 +125,7 @@ export default function EventModal(props: EventModalProps) {
               onChange={(e) => { setTitle(e.target.value); }}
               className="w-full border border-gray-300 rounded-lg px-3 py-2 text-base focus:outline-none focus:ring-2 focus:ring-blue-500"
               placeholder="Event title"
-              autoFocus
+              autoFocus={window.innerWidth > 768}
             />
           </div>
 
